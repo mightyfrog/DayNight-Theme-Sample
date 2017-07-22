142e9ab302837d22af92a4d170a074ff711660f1
mightyfrog <mightyfrog.gc@gmail.com>
2017-07-22 21:53:52 +0900

    Initial commit

---
+++ app/src/main/java/org/mightyfrog/android/daynighthemesample/MainActivity.java
@@ -0,0 +1,54 @@
+package org.mightyfrog.android.daynighthemesample;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+/**
+ * https://medium.com/@chrisbanes/appcompat-v23-2-daynight-d10f90c83e94
+ *
+ * @author Shigehiro Soejima
+ */
+public class MainActivity extends AppCompatActivity {
+    private SharedPreferences mPrefs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toggleDayNight();
+            }
+        });
+
+        mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (mPrefs.getBoolean("mode_night", true)) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+    }
+
+    private void toggleDayNight() {
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_NO) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            mPrefs.edit().putBoolean("mode_night", true).apply();
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            mPrefs.edit().putBoolean("mode_night", false).apply();
+        }
+
+        recreate();
+    }
+}
